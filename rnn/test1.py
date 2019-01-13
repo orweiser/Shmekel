4a6a51973be590f92184472a394692557f97cafc
cohenshmekel <cohen.shmekel@gmail.com>
2019-01-13 23:06:38 +0200

    training works for input->GRU->FC_>output problem with FC->GRU, shapes error

--- rnn/test1.py
+++ rnn/test1.py
@@ -1,5 +1,7 @@
 import sys, os
 import numpy as np
+import sys
+sys.path.insert(0, '..')
 from Indicators import Indicators as ind
 import keras as K
 from keras.models import Sequential
@@ -17,25 +19,29 @@ Google.load_data()
 x = Google.get_features_as_list(False, False, True)
 
 # till we fix indicators
-d_test = 100
 d_train = 2900
-n_test = np.zeros((d_test, len(x)))
-n_train = np.zeros((d_train, len(x)))
+d_test = 100
+x_train = np.zeros((d_train, len(x)))
+x_test = np.zeros((d_test, len(x)))
+
 for i in range(len(x)):
-    n_train[:, i] = x[i][-(d_train + d_test):-d_test]
-    n_test[:, i] = x[i][-d_test:]
+    x_train[:, i] = x[i][-(d_train + d_test):-d_test]
+    x_test[:, i] = x[i][-d_test:]
 ##
-input_size = n_train.shape[0]
+input_size = x_train.shape[1]
 output_size = 5  # candle size
-layer1_size = 256
+layer1_size = 6  # 256
 gru_size = 256
+
+# x_train = np.reshape(x_train, (x_train.shape[0], 1, x_train.shape[1]))
+# x_test = np.reshape(x_test, (x_test.shape[0], 1, x_test.shape[1]))
 
 model = Sequential()
 model.add(Dense(layer1_size, activation='relu', input_dim=input_size))
-# model.add(Dense(5, activation='sigmoid'))
-model.summary()
-model.add(layers.Flatten())
-model.summary()
+# model.summary()
+# model.add(layers.Flatten())
+# model.summary()
+# model.add(layers.Reshape([2900, 6, 1]))  # (x_train.shape[0], x_train.shape[1], 1)
 model.add(GRU(input_dim=layer1_size, output_dim=gru_size, activation='tanh', return_sequences=False))
 model.add(Dense(output_size, activation='relu', input_dim=gru_size))
 model.compile(optimizer='rmsprop',
@@ -43,5 +49,5 @@ model.compile(optimizer='rmsprop',
               metrics=['accuracy'])
 
 print('Start training...')
-model.fit(n_train, Google.data[-(d_train+d_test):-d_test, :], epochs=10, batch_size=32)
+model.fit(x_train, Google.data[-(d_train+d_test):-d_test, :], epochs=100, batch_size=32)
 print('Done training!')
