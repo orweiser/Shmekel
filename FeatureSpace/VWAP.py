f372e819c293027634e34c69e6a8caae803edba2
orweiser <weiser.shmekel@gmail.com>
2019-01-27 22:47:46 +0200

    Finished passing oringinal implementations to new file structure

---
+++ FeatureSpace/VWAP.py
@@ -0,0 +1,28 @@
+import numpy as np
+from ShmekelCore import Feature
+
+
+class VWAP(Feature):
+    def __init__(self, period=14, **kwargs):
+        """
+        use this method to define the parameters of the feature
+        """
+        super(VWAP, self).__init__(**kwargs)
+        self.time_delay = period
+
+    def _compute_feature(self, data):
+        """
+        VWAP (Volume Weighted Average Price)
+        data columns represent'Open', 'High', 'Low', 'Close', 'Volume'
+                                0       1       2       3         4
+        """
+        close = self._get_basic_feature(data[0], 'close')
+        volume = self._get_basic_feature(data[0], 'volume')
+
+        N = self.time_delay
+        M = data[0].shape[0]
+        vwap = np.zeros(M - N - 1)
+        vc = np.multiply(close, volume)
+        for i in range(M - N - 1):
+            vwap[i] = np.divide(sum(vc[i:i + N]), sum(volume[i:i + N]))
+        return vwap
