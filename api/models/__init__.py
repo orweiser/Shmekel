5ae2da4734de3c649a479993418709a6bf70f1cb
kaufmann <kaufmann.shmekel@gmail.com>
2019-03-06 00:24:22 +0200

    added LSTM model

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -1,9 +1,12 @@
 from .fully_connected import FullyConnected
+from .lstm import LSTM
 
 
 def get(model: str, **kwargs):
     if model == 'FullyConnected':
         return FullyConnected(model=model, **kwargs)
+    if model == 'LSTM':
+        return LSTM(model=model, **kwargs)
 
     raise ValueError('Unexpected model, got ' + model)
 
