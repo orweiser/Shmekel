eb747088ac18ca4753cbd3a90b6610dc5ebe860c
Maorarb <45874832+Maorarb@users.noreply.github.com>
2019-04-19 11:12:31 +0300

    fixes per orweiser
    fixes in SMA and WMA per orweiser review

--- feature_space/WMA.py
+++ feature_space/WMA.py
@@ -1,9 +1,9 @@
 import numpy as np
-from ShmekelCore import Feature
+from shmekel_core import Feature
 
-class SMA(Feature):
+class WMA(Feature):
     def __init__(self,range = 14, data=None, normalization_type=None, time_delay=0, is_numerical=None):
-        self.range = range
+        self._range = range
         self.data = data
         self.normalization_type = normalization_type
         self.is_numerical = 1
@@ -11,6 +11,6 @@ class SMA(Feature):
 
     def _compute_feature(self, data):
         close = self._get_basic_feature(data[0], 'close')
-        wArray = np.arange(0, self.range) + 1
+        wArray = np.arange(0, self._range) + 1
         weights = wArray / wArray.sum()
         return np.convolve(close, weights[::-1], mode='valid')
