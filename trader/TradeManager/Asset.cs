f5993e001f41ddf9ea078829de788346b67e7fd9
Maorarb <45874832+Maorarb@users.noreply.github.com>
2019-11-25 21:22:46 +0200

    Added trader CS project to Shmekel repo

---
+++ trader/TradeManager/Asset.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Globals;
+
+namespace TradeManager
+{
+    /// <summary>
+    /// Asset to predict and trade ( can be a stock or ETF )
+    /// </summary>
+    public class Asset
+    {
+        public double PredictUpThreshold = 0;// threshold for predictor classification as 1 / 0
+        public string CsvPath;//csv path of asset candle set with predictor column
+        public List<Candle> Candles = new List<Candle>();//main candle list 
+
+        public TimeSpan TimeGapThreshold = new TimeSpan(1);//minimum time span to indicate a time gap between candles that requires exit on close
+        
+        /// <summary>
+        /// Load Candles from csv to main candle list
+        /// </summary>
+        /// <param name="csvPath"></param>
+        public void LoadFromCsv(string csvPath)
+        {
+            this.CsvPath = csvPath;
+
+            //update csv indexes from input config
+            int dateTimeIndex = InputVariables.GlobalConfig.DateTimeIndex;
+            int openIndex = InputVariables.GlobalConfig.OpenIndex;
+            int highIndex = InputVariables.GlobalConfig.HighIndex;
+            int lowIndex = InputVariables.GlobalConfig.LowIndex;
+            int closeIndex = InputVariables.GlobalConfig.CloseIndex;
+            int volumeIndex = InputVariables.GlobalConfig.VolumeIndex;
+
+            int predictUpIndex = InputVariables.GlobalConfig.PredictUpIndex;
+
+            double previousClose = 0;
+
+            //load csv
+            var lines = File.ReadAllLines(csvPath);
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string[] split = lines[i].Split(',');
+                Candle candle = new Candle();
+
+                //raw data
+                candle.DateTime = Convert.ToDateTime(split[dateTimeIndex]);
+                candle.Open = Convert.ToDouble(split[openIndex]);
+                candle.High = Convert.ToDouble(split[highIndex]);
+                candle.Low = Convert.ToDouble(split[lowIndex]);
+                candle.Close = Convert.ToDouble(split[closeIndex]);
+                candle.Volume = Convert.ToDouble(split[volumeIndex]);
+
+                //prediction
+                candle.PredictUp = Convert.ToDouble(split[predictUpIndex]);
+                
+                //ratios
+                candle.OpenToCloseRatio = (candle.Close - candle.Open) / candle.Open;
+                candle.OpenToHighRatio = (candle.High - candle.Open) / candle.Open;
+                candle.OpenToLowRatio = (candle.Low - candle.Open) / candle.Open;
+                candle.PreviousClose = previousClose;
+                candle.PreviousCloseToOpenRatio = (candle.Open-previousClose)/previousClose;
+                
+                Candles.Add(candle);
+                //candle.print();
+                //Console.ReadKey();
+                previousClose = candle.Close;
+            }
+
+            //set TimeGap
+            SetTimeGapThreshold();
+            
+        }
+
+        /// <summary>
+        /// Train the asset trader:
+        /// 1 - initialize trader
+        /// 2 - run Learn function
+        /// 3 - save results 
+        /// </summary>
+        public void Train()
+        {
+            Logger.AddEntry("START TRAINING ASSET ",LogEntryType.Info);
+            UpSingleTrader trader = new UpSingleTrader(this.TimeGapThreshold,1000);
+            trader.Learn(this.Candles,InputVariables.GlobalConfig.TrainResolution);
+            Logger.AddEntry("FINISH TRAINING ASSET ", LogEntryType.Info);
+
+
+            testSave();
+
+            void testSave()
+            {
+
+                List<string> lines = new List<string>();
+                lines.Add(trader.CurrentStrategy.ToCsvHeader());
+                
+                foreach (Strategy selectedStrategy in trader.SelectedStrategies)
+                {
+                    lines.Add(selectedStrategy.ToCsvLine());
+                }
+             
+                File.WriteAllLines(InputVariables.OutputPath,lines);
+
+            }
+
+        }
+
+        /// <summary>
+        /// Set the time gap threshold for exit on close
+        /// </summary>
+        public void SetTimeGapThreshold()
+        {
+            try
+            {
+                List<DateTime> times = Candles.Select(x => x.DateTime).ToList();
+                Dictionary<TimeSpan, int> tsDictionary = new Dictionary<TimeSpan, int>();
+                for (int i = 1; i < times.Count; i++)
+                {
+                    TimeSpan span = times[i] - times[i - 1];
+
+                    if (tsDictionary.ContainsKey(span))
+                    {
+                        tsDictionary[span]++;
+                    }
+                    else
+                    {
+                        tsDictionary[span] = 1;
+                    }
+                }
+
+                int max = tsDictionary.Values.Max();
+                this.TimeGapThreshold = tsDictionary.Where(x => x.Value == max).Select(x => x.Key).First(); ;
+            }
+            catch (Exception e)
+            {
+                Logger.AddEntry("SET TimeGapThreshold FAILURE: "+e.Message,LogEntryType.Critical);
+            }
+           
+            
+        }
+
+    }
+
+}
---
+++ trader/TradeManager/ITrader.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Globals;
+
+namespace TradeManager
+{
+    interface ITrader
+    {
+        void Learn(List<Candle> iCandles,int resolution);
+        
+        void OfflineTrade(List<Candle> offlineCandles);
+    }
+
+    public class UpSingleTrader : ITrader
+    {
+        //strategy type 
+        public StrategyType StrategyType = StrategyType.Basic;
+
+        public Strategy CurrentStrategy;
+        public List<Strategy> Strategies=new List<Strategy>();
+        public List<Strategy> SelectedStrategies = new List<Strategy>();
+
+        public TimeSpan TimeGapThreshold;
+        public double InitialFunds;
+
+        public double Funds;
+        public double PreviousCloseToEntryRatio;
+        public double OpenToTakeProfitRatio;
+        public double OpenToStopLossRatio;
+
+        /// <summary>
+        /// basic trader - for candle up trading
+        /// predictor for this trader: 0 to 1 predicting if candle will close higher then open
+        /// </summary>
+        /// <param name="timeGapThreshold">TimeSpan type, defines the threshold for time gap (exit on close) </param>
+        /// <param name="initialFunds">initial funds for trader</param>
+        public UpSingleTrader(TimeSpan timeGapThreshold,double initialFunds = 0)
+        {
+            TimeGapThreshold = timeGapThreshold;
+            InitialFunds = initialFunds;
+        }
+
+        
+        /// <summary>
+        /// Learn the best strategy for a given set of candles
+        /// </summary>
+        /// <param name="iCandles">list of candles to learn</param>
+        /// <param name="resolution">Learn resolution - used for threshold, combinations </param>
+        public void Learn(List<Candle> iCandles,int resolution)
+        {
+            GenerateStrategies(resolution);
+            LearnStrategies(iCandles);
+
+            //Generate all strategy combinations
+            void GenerateStrategies(int slices)
+            {
+                List<double> predictorThresholds = new List<double>();
+                List<double> o2HValues = new List<double>();
+                List<double> o2LValues = new List<double>();
+                List<double> p2OValues = new List<double>();
+
+                var upCandles = iCandles.Where(x => x.Close > x.Open);
+
+                //get average ratios
+                var avgO2H = upCandles.Average(p => p.OpenToHighRatio);
+                var avgO2L = upCandles.Average(p => p.OpenToLowRatio);
+                var avgP2O = upCandles.Average(p => p.PreviousCloseToOpenRatio);
+
+                //**** set limits and fill arrays ****
+
+                //Thresholds
+                switch (StrategyType)
+                {
+                    case StrategyType.Basic:
+                        double thresholdStep = Convert.ToDouble(1) / slices;
+                        for (int i = 0; i < slices; i++)
+                        {
+                            predictorThresholds.Add(i*thresholdStep);
+                            //Console.WriteLine();
+                        }
+
+                        predictorThresholds.Remove(1);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                //OPEN TO HIGH
+                double maxO2H = avgO2H * 2;
+                double minO2H = avgO2H / 2;
+                double spanO2H = maxO2H - minO2H;
+                double stepO2H = spanO2H / slices;
+                for (int i = 0; i <= slices; i++){o2HValues.Add(minO2H+i*stepO2H);}
+               
+                //OPEN TO LOW
+                double maxO2L = avgO2L / 2;
+                double minO2L = avgO2L * 2;
+                double spanO2L = Math.Abs(maxO2L - minO2L);
+                double stepO2L = spanO2L / slices;
+                for (int i = 0; i <= slices; i++){o2LValues.Add(minO2L + i * stepO2L);}
+
+                //PREVIOUS TO OPEN
+                double maxP2O = avgP2O * 2;
+                double minP2O = avgP2O / 2;
+                double spanP2O = Math.Abs(maxP2O - minP2O);
+                double stepP2O = spanP2O / slices;
+                for (int i = 0; i <= slices; i++) { p2OValues.Add(minP2O + i * stepP2O); }
+
+                
+
+                for (int i = 0; i < o2HValues.Count; i++)
+                {
+                    for (int j = 0; j < o2LValues.Count; j++)
+                    {
+                        for (int k = 0; k < p2OValues.Count; k++)
+                        {
+                            foreach (double predictorThreshold in predictorThresholds)
+                            {
+
+                                Dictionary<StrategyRatioType, double> ratios = new Dictionary<StrategyRatioType, double>();
+                                ratios[StrategyRatioType.Entry] = p2OValues[k];
+                                ratios[StrategyRatioType.StopLoss] = o2LValues[j];
+                                ratios[StrategyRatioType.TakeProfit] = o2HValues[i];
+                                ratios[StrategyRatioType.PredictorThreshold] = predictorThreshold;
+
+                                Strategy strategy = new Strategy(StrategyType.Basic, ratios);
+                                strategy.Score.TotalFunds = InitialFunds;
+                                strategy.Score.InitialFunds = InitialFunds;
+                                Strategies.Add(strategy);
+                               
+                            }
+
+                            
+                        }
+
+                    }
+                }
+               
+            }
+            
+            //learn all strategies by offline trade
+            void LearnStrategies(List<Candle> candles)
+            {
+                foreach (Strategy strategy in Strategies)
+                {
+                    CurrentStrategy = strategy;
+                    OfflineTrade(candles);
+                    CurrentStrategy.UpdateScore();
+                    
+                    if (CurrentStrategy.Score.TotalFunds>InitialFunds)
+                    {
+                        SelectedStrategies.Add(CurrentStrategy);
+                        Logger.AddEntry("Strategy added to selected, total funds: " + CurrentStrategy.Score.TotalFunds, LogEntryType.Info);
+
+                    }
+                    else
+                    {
+                        Logger.AddEntry("Strategy failed, total funds: "+CurrentStrategy.Score.TotalFunds,LogEntryType.Info);
+                    }
+                    
+                    //Console.WriteLine(strategy.ToCsvLine());
+                    // Console.ReadKey();
+                }
+            }
+            
+        }
+
+
+
+
+        public void OfflineTrade(List<Candle> offlineCandles)
+        {
+            //grace period - minimum candles before checking if strategy is a loosing strategy 
+            double gracePeriodCandle = Math.Round(offlineCandles.Count*InputVariables.GlobalConfig.GracePeriodRatio);
+
+            for (int i = 0; i < offlineCandles.Count-CurrentStrategy.MaxCandlesPerTrade; i++)
+            {
+                //check predict up for trade open
+                CurrentStrategy.Score.TotalCandleCount++;
+                if (offlineCandles[i].PredictUp <= CurrentStrategy.Ratios[StrategyRatioType.PredictorThreshold])
+                {
+                    continue;
+                }
+
+                //Check for grace period status
+                if (i >gracePeriodCandle)
+                {
+                    if (CurrentStrategy.Score.TotalFunds<InitialFunds)
+                    {
+                        //Console.WriteLine();
+                        Logger.AddEntry("NEGATIVE FUNDS AFTER GRACE PERIOD, EXIT OFFLINE TRADE", LogEntryType.Info);
+                        break;
+                    }
+                    
+
+                }
+                
+                //open trade
+                SimpleTrade trade = new SimpleTrade(TimeGapThreshold);
+                trade.Shares = 1000;
+                trade.TradeCandles = offlineCandles.GetRange(i, CurrentStrategy.MaxCandlesPerTrade);
+                trade.EntryPrice = offlineCandles[i].PreviousClose * (1 + CurrentStrategy.Ratios[StrategyRatioType.Entry]);
+                trade.TakeProfitPrice = offlineCandles[i].PreviousClose * (1 + CurrentStrategy.Ratios[StrategyRatioType.TakeProfit]);
+                trade.StopLossPrice = offlineCandles[i].PreviousClose * (1 + CurrentStrategy.Ratios[StrategyRatioType.StopLoss]);
+
+                //run offline
+                trade.Offline();
+                
+                //update strategy
+                CurrentStrategy.UpdateTrade(trade.PL,trade.Status);
+
+                //output score
+                if (InputVariables.GlobalConfig.OutputLevel == OutputLevel.Trade)
+                {
+                    Console.WriteLine(i + " | {0} , PL: {1}", trade.Status, trade.PL);
+                    Console.WriteLine("funds: {0} ", CurrentStrategy.Score.TotalFunds);
+                }
+
+            }
+
+        }
+
+        
+
+    }
+
+}
