182f6d5844c36541aa5ace28e888b63d6f0e0499
kaufmann <kaufmann.shmekel@gmail.com>
2020-04-24 19:39:07 +0300

    for maor

--- feature_space/rsi.py
+++ feature_space/rsi.py
@@ -14,17 +14,32 @@ class RSI(Feature):
         return self.process(close)
 
     def process(self, close):
-        dif = -np.diff(close)
-        u = np.maximum(dif, 0)
-        d = np.maximum(-dif, 0)
+        dif = np.diff(close) / close[:-1]
+        up = np.maximum(dif, 0)
+        down = -np.minimum(dif, 0)
 
-        smmau = math.smooth_moving_avg(u, self.range)
-        smmad = math.smooth_moving_avg(d, self.range)
+        avg_up = self.rsi_sma(up)
+        avg_down = self.rsi_sma(down)
 
-        rs = smmau / (smmad + math.EPS_DOUBLE)  # adding epsilon for numerical purposes
+        rs = avg_up / (avg_down + math.EPS_DOUBLE)  # adding epsilon for numerical purposes
         rsi = 100 - 100 / (1 + rs)
-
+        # if self.auto_fill:
+        #     delta = len(close) - len(rsi)
+        #     fill = np.full(delta, rsi[0])
+        #     rsi = np.concatenate([fill, rsi])
         return rsi
+
+    def rsi_sma(self, x):
+        smma_out = []
+        init_avg = np.average(x[0:self.range])
+        smma_out.append(init_avg)
+        multiplyer = self.range - 1
+        for xval in x[self.range:]:
+            last_val = smma_out[-1]
+            new_val = (multiplyer * last_val + xval) / self.range
+            smma_out.append(new_val)
+            # print(new_val)
+        return np.asarray(smma_out)
 
 
 class RSIOld(Feature):
