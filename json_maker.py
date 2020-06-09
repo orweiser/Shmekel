38fa73bb172a631d791153dab3e3ac0dc2d0d4e2
rotem perel <motarotem@gmail.com>
2020-06-09 20:50:12 +0300

    added dropout to  model config

--- json_maker.py
+++ json_maker.py
@@ -16,7 +16,7 @@ default_loss_config = {
 default_train_config = {
     "batch_size": 1024,
     "callbacks": None,
-    "epochs": 20,
+    "epochs": 200,
     "include_experiment_callbacks": True,
     "optimizer": "adam",
     "randomize": True,
@@ -24,8 +24,10 @@ default_train_config = {
     "validation_steps": None
 }
 default_backup_config = dict(handler='DefaultLocal')
-default_train_dataset_config = dict(dataset='StocksDataset', val_mode=False, time_sample_length=7)
-default_val_dataset_config = dict(dataset='StocksDataset', val_mode=True, time_sample_length=7)
+default_train_dataset_config = dict(dataset='StocksDataset', val_mode=False, time_sample_length=7,
+                                    stock_name_list=['fb'])
+default_val_dataset_config = dict(dataset='StocksDataset', val_mode=True, time_sample_length=7,
+                                  stock_name_list=['fb'])
 
 
 DEFAULT = dict(
@@ -38,7 +40,7 @@ DEFAULT = dict(
 )
 
 
-def _create_config(output_activation, layers_def, early_stop=True):
+def _create_config(output_activation, layers_def, early_stop=True, dropout=True, dropout_rate=0.2):
     # neurons_combination[index]):
 
     depth = len(layers_def)
@@ -47,7 +49,9 @@ def _create_config(output_activation, layers_def, early_stop=True):
         'num_of_layers': depth,
         'layers': [],
         'num_of_rnn_layers': 0,
-        "output_activation": output_activation
+        "output_activation": output_activation,
+        'dropout': dropout,
+        'dropout_rate': dropout_rate
     }
 
     for index, (layer_type, size, activation) in enumerate(layers_def):
@@ -74,10 +78,11 @@ def get_random_sample(layers_types, activation_functions, depths, neurons):
     return [map(choice, [layers_types, neurons, activation_functions]) for _ in range(depth)]
 
 
-def generate_grid_model(layers_types=(LSTM, DENSE), activation_functions=('relu', 'sigmoid', 'tanh'),
-                        depth=(3, 5, 10, 15, 20), neurons=(8, 32, 128), output_activation='softmax', early_stop=True):
+def generate_grid_model(layers_types=(DENSE, ), activation_functions=('relu', 'sigmoid', 'tanh'),
+                        depth=(3, 5, 10, 15, 20), neurons=(8, 32, 128), output_activation='softmax',
+                        dropout=True, dropout_rate=0.2, early_stop=True):
     combs = get_random_sample(layers_types, activation_functions, depth, neurons)
-    return _create_config(output_activation, combs, early_stop=early_stop)
+    return _create_config(output_activation, combs, early_stop=early_stop, dropout=dropout, dropout_rate=dropout_rate)
 
 
 def grid_jason_maker(amount_of_experiments=-1, run_experiments=True, defaults_override=None, **kwargs):
@@ -122,86 +127,26 @@ def give_nickname(name):
     return name
 
 
-# def print_statistics(path, compare, fixed_values={}, metric='val_acc', file=None):
-#     results = {}
-#     for dir in os.listdir(path):
-#         if 'histories' in os.listdir(os.path.join(path, dir)):
-#             config = load_config(os.path.join(path, dir))
-#             exp = get_exp_from_config(config)
-#             config = load_config(os.path.join(path, '1563481881', 'config_' + dir + '.json'))
-#             identifiers = config.pop('identifiers')
-#             exp.identifiers = identifiers
-#             is_in_fixed_values = True
-#             for key, value in fixed_values.items():
-#                 is_in_fixed_values = exp.identifiers[key] == value
-#             if is_in_fixed_values:
-#                 if compare == "size":
-#                     size = 0
-#                     for layer in range(exp.identifiers['num_of_layers']):
-#                         size += exp.identifiers['layer_{layer}_size'.format(layer=layer)]
-#                     if size not in results:
-#                         results[size] = []
-#                     results[size].append(exp.results.get_best_epoch()[metric])
-#                 else:
-#                     if exp.identifiers[compare] not in results:
-#                         results[exp.identifiers[compare]] = []
-#                     results[exp.identifiers[compare]].append(exp.results.get_best_epoch()[metric])
-#
-#     for key, value in results.items():
-#         best = np.max(value)
-#         size = len(value)
-#         avg = np.mean(value)
-#         worst = np.min(value)
-#         print('Slice - {compare} {key}'.format(compare=compare, key=str(key)))
-#         print('total nets: %d;  max: %f;   avg: %f; min: %f' % (size, best, avg, worst))
-#         if file:
-#             os.system('echo Slice - {compare} {key} > {file}'.format(compare=compare, key=str(key),
-#                                                                      file=os.path.join(os.pardir, file)))
-#             print('echo total nets: %d;  max: %f;   avg: %f; min: %f > %s' % (
-#                 size, best, avg, worst, os.path.join(os.pardir, file)))
-
-
-
-
-# # for exp in gs.iter_modulo(rem=2):
-# for exp_name in exp_results['name']:
-#     config = load_config(os.path.join(config_path, 'config_' + exp_name + '.json'))
-#     config.pop('identifiers')
-#     exp = get_exp_from_config(config)
-#     exp.run()
-#     for m in metric:
-#         num = exp.results.get_best_epoch_number()
-#         val = exp.results.get_best_epoch()[m]
-#         labels_value_dict = {'best epoch numebr by {metric}'.format(metric=m): num,
-#                              'best epoch values by {metric}'.format(metric=m): val}
-#         idx = exp_results.loc[exp_results['name'] == exp._name].index.values.astype(int)[0]
-#         if exp_results.at[idx, 'status'] != 'Done':
-#             insert_values_to_csv_cells(grid_results_path, find_by='name', row_tags=[exp._name], data=labels_value_dict)
-#
-# # main
-# # print_statistics('C:\\Shmekel\\local_repository\\Shmekel_Results\\default_project', 'size',
-# #                  file='results.txt')
-
 if __name__ == '__main__':
 
     # Set version name and parameters to create a new models group
-    VERSION = 'version-0.0.1'
+    VERSION = 'version-0.0.3'
     config_path = os.path.join(os.pardir, 'Shmekel_Results', VERSION)
 
-    depth_list = (3, 5, 10, 15, 20)
+    depth_list = (3, 5)
+    neurons_list = (8, 32)
+    activation_functions = ('relu', 'tanh')
+    dropout = False
+    dropout_rate = 0.2
     version_parameters = {
         "EXP_CONFIG": None,
-        "MIN_SIZE": 3,
-        "MAX_SIZE": 7,
         "MAX_NUM_OF_LAYERS": max(depth_list),
-        "LAYERS_TYPES": ['KerasLSTM', 'Dense'],
+        "LAYERS_TYPES": ['Dense'],
         "ALL_DENSE_SAME": False,
-        "ACTIVATION_FUNCTIONS": ['relu', 'sigmoid', 'tanh'],
-        "DROPOUT_CHANCE": 0.7,
-        "MIN_DROPOUT": 0.1,
-        "MAX_DROPOUT": 0.3,
-        "MAX_DEPTH": 20,
-        "NUM_OF_EPOCHS": 20
+        "ACTIVATION_FUNCTIONS": activation_functions,
+        'DROPOUT': dropout,
+        "DROPOUT_RATE": dropout_rate,
+        "NUM_OF_EPOCHS": default_train_config['epochs']
     }
 
     # Save version config in version folder
@@ -210,5 +155,6 @@ if __name__ == '__main__':
             json.dump(version_parameters, outfile)
 
     # Run version
-    grid_jason_maker(depth=depth_list)
+    grid_jason_maker(depth=depth_list, neurons=neurons_list, activation_functions=activation_functions,
+                     dropout=dropout, dropout_rate=dropout_rate)
 
