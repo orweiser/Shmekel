6af82f7b8875fecaf25accdcc8c69381205e790c
vimuzumu <michaelm3@gmail.com>
2020-08-02 22:19:10 +0300

    plot by attribute

--- api/scripts/analyze_exp.py
+++ api/scripts/analyze_exp.py
@@ -3,9 +3,21 @@ import os
 import sys
 
 import matplotlib.pyplot as plt
+from api.core import get_exp_from_config, load_config
 
 sys.path.insert(0, os.path.join('..', '..', '..', __file__))
-from api.core import get_exp_from_config, load_config
+
+
+def _get_attribute_from_config(config, attr):
+    print(attr)
+    attr_keys = attr.split("-")
+    for key in attr_keys:
+        if key in config:
+            config = config[key]
+        else:
+            raise Exception("attribute " + attr + " doesn't exist in config")
+
+    return config
 
 
 def _print_exp_metric(exp, metric):
@@ -14,19 +26,30 @@ def _print_exp_metric(exp, metric):
     exp.results.get_best_epoch(metric).print()
 
 
-def main(paths, metrics=None):
+def main(paths, metrics=None, attr=None):
     exps = [get_exp_from_config(load_config(path)) for path in paths]
 
     metrics = metrics or ['val_acc']
 
     legend = []
     plt.figure()
-    for exp in exps:
+    if attr:
         for metric in metrics:
-            plt.plot(range(1, len(exp.results[metric]) + 1), exp.results[metric])
-            legend.append(f'{os.path.basename(exp.name).rsplit(".", 1)[0]}--{metric}')
+            x = []
+            y = []
+            for exp in exps:
+                x.append(_get_attribute_from_config(exp.config, attr))
+                y.append(exp.results.get_best_epoch(metric).scores[metric])
+            plt.plot(x, y)
+            legend.append(f'{attr}--{metric}')
+        plt.xlabel(attr)
+    else:
+        for exp in exps:
+            for metric in metrics:
+                plt.plot(range(1, len(exp.results[metric]) + 1), exp.results[metric])
+                legend.append(f'{os.path.basename(exp.name).rsplit(".", 1)[0]}--{metric}')
+        plt.xlabel('#Epoch')
     plt.legend(legend)
-    plt.xlabel('#Epoch')
     plt.title('Learning Curves Comparison')
     plt.grid()
     plt.show()
@@ -41,7 +64,8 @@ if __name__ == '__main__':
 
     parser.add_argument('config_paths', nargs='+')
     parser.add_argument('--metrics', nargs='*', type=str, help='metrics to plot')
+    parser.add_argument('--attr', type=str, help='plot best epoch metrics by attribute')
     args = parser.parse_args()
 
     params = {}
-    main(args.config_paths, metrics=args.metrics)
+    main(args.config_paths, metrics=args.metrics, attr=args.attr)
