e6d0d88929235b9c0ca7e1c9e6ac32fd1af6e27d
Rotem <perel.shmekel@gmail.com>
2020-08-29 18:27:15 +0300

    copy of macd2 after correction

--- feature_space/macd.py
+++ feature_space/macd.py
@@ -1,10 +1,10 @@
 import numpy as np
 from shmekel_core import Feature
-from shmekel_core.math import exponential_moving_average
+from shmekel_core.math import exponential_moving_average_investopedia
 
 
 class MACD(Feature):
-    def __init__(self, calc_signal_line=False, **kwargs):
+    def __init__(self, calc_signal_line=False, normalization_type='default'):
         """
         MACD is defined as : EMA_12(data) - EMA_26(data),  where EMA_x(data) is the x period EMA calculated on data.
         The signal line is the EMA_9(MACD) and is usually used as a trigger for buy and sell signals
@@ -12,26 +12,33 @@ class MACD(Feature):
         time_delay of the feature.
         :type calc_signal_line: bool
         """
-        super(MACD, self).__init__(**kwargs)
+        super(MACD, self).__init__(normalization_type=normalization_type, is_numerical=True)
         self._calc_signal_line = calc_signal_line
 
-        self.time_delay = 26
+        self.range1 = 12
+        self.range2 = 26
+        self.range_signal = 9
+        self.time_delay = self.range2
+        self.num_features = 1
         if calc_signal_line:
-            self.time_delay += 9
+            self.time_delay += self.range_signal
             self.num_features = 2
 
     def _compute_feature(self, data, feature_list=None):
+
         close = self._get_basic_feature(data[0], 'close')
-        low = self._get_basic_feature(data[0], 'low')
-        high = self._get_basic_feature(data[0], 'high')
+        return self.process(close)
 
-        price = (high + low + close) / 3.
-        ema_12 = exponential_moving_average(price, 12)
-        ema_26 = exponential_moving_average(price, 26)
+    def process(self, close): #  Should we use "close" or "typical price" (typical price = (close + low + high)/3 )
 
-        # todo: fix. the arrays below have different shapes and cannot be summed
-        macd = ema_12[:14] - ema_26
+        range1, range2, range_s = self.range1, self.range2, self.range_signal
+        ema_12 = exponential_moving_average_investopedia(close, range1)
+        ema_26 = exponential_moving_average_investopedia(close, range2)
+        macd = ema_12[range2 - range1:] - ema_26
+
         if self._calc_signal_line:
-            signal_line = exponential_moving_average(macd, 9)
-            return np.concatenate((macd, signal_line), axis=1)
+            signal_line = exponential_moving_average_investopedia(macd, range_s)
+            macd_plus_signal = np.concatenate((np.expand_dims(macd[range_s - 1:], 1), np.expand_dims(signal_line, 1)), axis=1)
+            return macd_plus_signal
+
         return macd
