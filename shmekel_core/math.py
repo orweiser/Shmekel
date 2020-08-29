c8ec7f68c46a938135dee95f20e9d2b92b79b8c6
Rotem <perel.shmekel@gmail.com>
2020-08-29 18:21:58 +0300

    added two functions to calculate EMA and SMA according to investopia calculation

--- shmekel_core/math.py
+++ shmekel_core/math.py
@@ -28,3 +28,24 @@ def exponential_moving_average(data_seq, period):
         ema[-i] = data_seq[len(ma) - i] * ema[-(i + 1)] * (1 - mult)
 
     return ema
+
+
+def smooth_moving_avg_investopedia(data_seq, period):
+    sma = np.zeros(data_seq.shape[0] - period + 1)
+    sma_size = np.size(sma)
+    for i in range(period):
+        sma = sma + data_seq[i: i + sma_size]
+
+    sma = sma / period
+    return sma
+
+
+def exponential_moving_average_investopedia(data_seq, period):
+    Smoothing = 2
+    ema_factor = Smoothing / (1 + period)
+    ema = np.zeros(data_seq.shape[0] - period + 1)
+    ema[0] = np.sum(data_seq[:period]) / period  # initialize first e
+    for i in range(1, len(ema)):
+        ema[i] = data_seq[i+period-1]*ema_factor + ema[i-1]*(1 - ema_factor)
+
+    return ema
