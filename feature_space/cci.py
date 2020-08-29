087fba7dc0110f5ef476659acb3fb8caa5beafb8
Rotem <perel.shmekel@gmail.com>
2020-08-29 20:26:12 +0300

    time_delay - 1

--- feature_space/cci.py
+++ feature_space/cci.py
@@ -4,7 +4,7 @@ from shmekel_core import Feature, math
 
 class CCI(Feature):
     def __init__(self, range=14, normalization_type='default'):
-        super(CCI, self).__init__(normalization_type=normalization_type, is_numerical=True, time_delay=range)
+        super(CCI, self).__init__(normalization_type=normalization_type, is_numerical=True, time_delay=range-1)
 
         self.range = range
 
@@ -19,7 +19,7 @@ class CCI(Feature):
         typical_price = (high + low + close) / 3
         ma = math.smooth_moving_avg_investopedia(typical_price, self.range)
         ma_size = np.size(ma)
-        deviation = np.zeros((ma_size, 1))
+        deviation = np.zeros((ma_size))
         for i in range(ma_size):
             ma_abs_val = np.abs(typical_price[i: i+self.range] - ma[i])
             deviation[i] = np.sum(ma_abs_val) / self.range  # range - 1 might be more suitable here
