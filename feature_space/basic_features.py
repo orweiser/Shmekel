af4092b3ab4a88f02247abeeb49f749bd23c2826
Rotem <perel.shmekel@gmail.com>
2020-10-07 23:40:33 +0300

    added AltCandle class

--- feature_space/basic_features.py
+++ feature_space/basic_features.py
@@ -33,6 +33,31 @@ class RawCandle(Candle):
         self.num_features = 5
 
 
+class AltCandle(Feature):
+    def __init__(self, with_volume=True, time_delay=0, **kwargs):
+        super(AltCandle, self).__init__(**kwargs)
+
+        self.is_numerical = True
+        self.time_delay = time_delay
+        self.with_volume = with_volume
+        self.num_features = 5 if with_volume else 4
+
+    def _compute_feature(self, data, feature_list=None, ):
+        if self.with_volume:
+            for i in [0, 1, 3]:
+                data[0][:, i] = np.divide(data[0][:, i], data[0][:, 2])
+            return data[0][self.time_delay:]
+
+        a = np.swapaxes(data[0], 0, self._feature_axis)
+        ind = [i for i in range(len(self._pattern)) if i != self._pattern.index('volume')]
+        a = a[ind]
+
+        a = np.swapaxes(a, 0, self._feature_axis)
+        for i in [0, 1, 3]:
+            a[0][:, i] = np.divide(a[0][:, i], a[0][:, 2])
+        return a[self.time_delay:]
+
+
 class DateTuple(Feature):
     def __init__(self, normalization_type=None, **kwargs):
         super(DateTuple, self).__init__(normalization_type=normalization_type, **kwargs)
